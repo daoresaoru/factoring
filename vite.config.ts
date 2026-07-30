547e66ece4b83fadb9029061c24ff299b526e0d6
daoresaoru <149461277+daoresaoru@users.noreply.github.com>
2026-07-30 16:06:18 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/factoring/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
